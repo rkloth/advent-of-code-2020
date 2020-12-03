29fef564f66eb023fc8d17419b0666ebdbe04005
Ricardo Kloth <ricardo.kloth@mediaan.com>
2020-12-03 10:14:32 +0100

    adds assignment as comment for day 2

--- days/02/index.js
+++ days/02/index.js
@@ -41,13 +41,28 @@ const down_the_road_policy = ({ password, character, a, b}) => {
   return (matches.length >= a && matches.length <= b);
 };
 
-const official_policy = ({ password, character, a, b }) => {
-  return password.charAt(a) === character ^ password.charAt(b) === character;
-};
-
 const first = (input) => {
   return input.reduce((n, record) => n +(+down_the_road_policy(collect(record))), 0);
 }
+
+/*
+  Each policy actually describes two positions in the password, where 1 means the first character,
+  2 means the second character, and so on.
+  (Be careful; Toboggan Corporate Policies have no concept of "index zero"!)
+  Exactly one of these positions must contain the given letter.
+  Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+  Given the same example list from above:
+
+  1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+  1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+  2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+  How many passwords are valid according to the new interpretation of the policies?
+*/
+
+const official_policy = ({ password, character, a, b }) => {
+  return password.charAt(a) === character ^ password.charAt(b) === character;
+};
 
 const second = (input) => {
   return input.reduce((n, record) => n +(+official_policy(collect(record))), 0);
