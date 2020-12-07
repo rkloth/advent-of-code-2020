8d9c3bc0e6f3d9ee93d6abd57d73933ef3b6787f
Ricardo Kloth <ricardo.kloth@mediaan.com>
2020-12-07 20:49:09 +0100

    implements day 6

--- days/04/index.js
+++ days/04/index.js
@@ -1,5 +1,5 @@
 /*
-  https://adventofcode.com/2020/day/3
+  https://adventofcode.com/2020/day/4
 */
 
 const fs = require("fs");
---
+++ days/06/index.js
@@ -0,0 +1,41 @@
+/*
+  https://adventofcode.com/2020/day/3
+*/
+
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(__dirname + "/input.txt", "utf8")
+  .replace(/\r/g, "")
+  .replace(" ", "")
+  .split("\n\n")
+  .map((line) => line.split("\n").filter(Boolean));
+
+
+const uniqueItemsFrom = (items) => [...new Set(items)];
+const mergeArrays = (arrays) => arrays.reduce((a, b) => [...a, ...b]);
+const allArraysContain = (value, arrays) =>
+  arrays.every((a) => a.includes(value));
+
+const nrOfIntersections = (uniqueItems, arrays) => {
+  return uniqueItems.reduce((count, value) => {
+    return count + +allArraysContain(value, arrays);
+  }, 0);
+};
+
+const first = (input) => {
+  return input.reduce((count, groupAnswers) => {
+    return count + uniqueItemsFrom(mergeArrays(groupAnswers)).length;
+  }, 0);
+};
+
+const second = (input) => {
+  return input.reduce((count, groupAnswers) => {
+    const uniqueItems = uniqueItemsFrom(mergeArrays(groupAnswers));
+
+    return count + nrOfIntersections(uniqueItems, groupAnswers);
+  }, 0);
+};
+
+console.log("solution first", first(input));
+console.log("solution second", second(input));
